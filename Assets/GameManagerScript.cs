f3366a88acaa7705f3aaa30d24796ed501f16d93
miyazakiyamato <146906497+miyazakiyamato@users.noreply.github.com>
2024-04-16 17:18:38 +0900

    02_01

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -4,47 +4,91 @@ using UnityEngine;
 
 public class GameManagerScript : MonoBehaviour
 {
-    int[] map;
-
-
-    // Start is called before the first frame update
-    void Start()
+    int[,] map;
+    void PrintArray()
     {
-        map = new int[] { 0,0,0,1,0,0,0,0,0};
-        //Debug.Log("Hello World");
         string debugText = "";
-        for(int i = 0;i < map.Length; i++)
+        for (int y = 0; y < map.GetLength(0); y++)
         {
-            debugText += map[i].ToString() + ",";
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                debugText += map[y,x].ToString() + ",";
+            }
+            debugText += "\n";
         }
         Debug.Log(debugText);
     }
+    int GetPlayerIndexX()
+    {
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for(int x = 0;x < map.GetLength(1); x++)
+            {
+                if (map[y,x] == 1)
+                {
+                    return x;
+                }
+            }
+           
+        }
+        return -1;
+    }
+    int GetPlayerIndexY()
+    {
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    return y;
+                }
+            }
 
+        }
+        return -1;
+    }
+    bool MoveNumberX(int number, int moveFromX, int moveToX,int moveFromY)
+    {
+        if (moveToX < 0 || moveToX >= map.GetLength(1)) { return false; }
+        if (map[moveFromY,moveToX] == 2)
+        {
+            int velocity = moveToX - moveFromX;
+
+            bool success = MoveNumberX(2, moveToX, moveToX + velocity,moveFromY);
+            if (!success) { return false; }
+        }
+
+        map[moveFromY,moveToX] = number;
+        map[moveFromY,moveFromX] = 0;
+        return true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new int[,] {
+            { 0,0,0,0,0},
+            { 0,0,1,0,0},
+            { 0,0,0,0,0},
+        };
+        PrintArray();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            int playerIndex = -1;
-            for(int i = 0;i < map.Length;i++)
-            {
-                if (map[i] == 1)
-                {
-                    playerIndex = i;
-                    break;
-                }
-            }
-            if(playerIndex < map.Length - 1)
-            {
-                map[playerIndex + 1] = 1;
-                map[playerIndex] = 0;
-            }
-            string debugText = "";
-            for (int i = 0; i < map.Length; i++)
-            {
-                debugText += map[i].ToString() + ",";
-            }
-            Debug.Log(debugText);
+            int playerIndexX = GetPlayerIndexX();
+            int playerIndexY = GetPlayerIndexY();
+            MoveNumberX(1, playerIndexX, playerIndexX + 1,playerIndexY);
+            PrintArray();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            int playerIndexX = GetPlayerIndexX();
+            int playerIndexY = GetPlayerIndexY();
+            MoveNumberX(1, playerIndexX, playerIndexX - 1, playerIndexY);
+            PrintArray();
         }
     }
 }
